04bff15421c68e88c8d8a41a0d4b4d642e5888b4
Kang Duoh <kduoh99@gmail.com>
2025-04-26 09:47:43 +0900

    Feat: 재고감소 로직 작성

---
+++ src/main/java/com/study/stock/service/StockService.java
@@ -0,0 +1,25 @@
+package com.study.stock.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.study.stock.domain.Stock;
+import com.study.stock.repository.StockRepository;
+
+@Service
+public class StockService {
+
+	private final StockRepository stockRepository;
+
+	public StockService(StockRepository stockRepository) {
+		this.stockRepository = stockRepository;
+	}
+
+	@Transactional
+	public void decrease(Long id, Long quantity) {
+		Stock stock = stockRepository.findById(id).orElseThrow();
+		stock.decrease(quantity);
+
+		stockRepository.saveAndFlush(stock);
+	}
+}
