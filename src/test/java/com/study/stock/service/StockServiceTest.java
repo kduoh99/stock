e8bb1a02c1de0179a025d94edc3746870e560b91
Kang Duoh <kduoh99@gmail.com>
2025-04-26 10:05:02 +0900

    Test: 문제점

--- src/test/java/com/study/stock/service/StockServiceTest.java
+++ src/test/java/com/study/stock/service/StockServiceTest.java
@@ -2,6 +2,10 @@ package com.study.stock.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,5 +42,30 @@ public class StockServiceTest {
 		Stock stock = stockRepository.findById(1L).orElseThrow();
 
 		assertEquals(99, stock.getQuantity());
+	}
+
+	// Race Condition
+	@Test
+	public void 동시에_100개_요청() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		// 100 - (1 * 100) = 0
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		assertEquals(0, stock.getQuantity());
 	}
 }
